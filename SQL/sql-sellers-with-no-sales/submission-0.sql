e6d10745c97c5bab0587822a5f9569ba337b2506
Kaden Primorac <kadenpt@gmail.com>
2026-09-15 22:49:08 -0700

    Add: sql-sellers-with-no-sales - submission-0

---
+++ SQL/sql-sellers-with-no-sales/submission-0.sql
@@ -0,0 +1,3 @@
+-- Write your query below
+select seller_name from seller where
+seller_id not in (select seller_id from orders where sale_date >= '2020-01-01' AND sale_date <= '2020-12-31') order by seller_name asc
